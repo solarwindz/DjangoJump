b19a51bb5dca4e0d313d777f751dbc640db654b7
Aaron Zhang <johnson_uestc@126.com>
2019-04-29 10:54:26 +0800

    Add music code

--- django_jump/django_jump/urls.py
+++ django_jump/django_jump/urls.py
@@ -1,7 +1,7 @@
-"""django_jump URL Configuration
+"""music URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+from django.views import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('index.urls')),
+    path('ranking.html', include('ranking.urls')),
+    path('play/', include('play.urls')),
+    path('comment/', include('comment.urls')),
+    path('search/', include('search.urls')),
+    path('user/', include('user.urls')),
+    # 设置项目上线的静态资源路径
+    url('^static/(?P<path>.*)$', static.serve,
+        {'document_root': settings.STATIC_ROOT}, name='static')
 ]
+
+
+# 设置404、500错误状态码
+from index import views
+handler404 = views.page_not_found
+handler500 = views.page_not_found
